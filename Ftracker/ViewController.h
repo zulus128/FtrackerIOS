71a991d65ac1ebbdf53882b791ee746972c64d87
Vadim Kassin <vgzulus@gmail.com>
2015-06-02 02:32:26 -0700

    3

--- Ftracker/ViewController.h
+++ Ftracker/ViewController.h
@@ -7,8 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate> {
+    
+    CLLocationManager *lm; //core lcoation manager instance
+    
+}
 
 @property (weak, nonatomic) IBOutlet UILabel *idLabel;
 
